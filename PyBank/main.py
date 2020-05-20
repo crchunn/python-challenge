a8f300326e91374676736c9aba964d4d8cb21054
Carrie Chunn <crchunn@gmail.com>
2020-05-20 12:22:43 -0500

    updated files

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,7 +3,30 @@ import os
 
 import csv
 
+import statistics
+
 csvpath = os.path.join("Resources/budget_data.csv")
+
+def print_percentages(profit_loss):
+    months = str(profit_loss[0])
+    amounts = int(profit_loss[1])
+
+    total_months = sum(months)
+
+    changes_in_profits = average(months)
+
+
+
+
+
+#data1 = [1, 3, 4, 5, 7, 9, 2] 
+  
+x = statistics.mean(profit_loss) 
+  
+# Printing the mean 
+print("Mean is :", x) 
+
+
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
@@ -15,15 +38,15 @@ with open(csvpath) as csvfile:
     for row in csvreader:
         print(row)
 
-import statistics
 
-#def print_percentages(profit_loss)
+
+
 
 #total number of months included in dataset
 
 #net total amount of profit/loss over entire period
 
-#df["Profit/Losses"].sum()
+    #df["Profit/Losses"].sum()
     #csvreader.next() 
     # to skip the header 
 
