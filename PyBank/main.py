bf925946b199e0d0d58afd61d288a478163d2bda
Carrie Chunn <crchunn@gmail.com>
2020-05-21 18:04:37 -0500

    changes to code to find the name

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,42 +5,75 @@ import csv
 
 import statistics
 
+print("Financial Analysis")
+
 csvpath = os.path.join("Resources/budget_data.csv")
 
-def print_percentages(profit_loss):
-    months = str(profit_loss[0])
-    amounts = int(profit_loss[1])
-
-    total_months = sum(months)
-
-    changes_in_profits = average(months)
 
 
+    #months = str(profit_loss[0])
+    #amounts = int(profit_loss[1])
+   
 
 
+    #changes_in_profits = average(months)
+
+
+
+
+#def print_percentages(profit_loss):
 
 #data1 = [1, 3, 4, 5, 7, 9, 2] 
   
-x = statistics.mean(profit_loss) 
+#x = statistics.mean(profit_loss) 
   
 # Printing the mean 
-print("Mean is :", x) 
-
+#print("Mean is :", x) 
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-
-    print(csvreader)
+    
+    greatestincrease = ["", 0]
+    greatestdecrease = ["", 999999]
+    avg_of_profit = []
+    total_months = 1
+    #print(csvreader)
 
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
+
+
+    net = next(csvreader)
+    net = int(net[1])
+    #print(net)
+    changes_in_profits = net
 
     for row in csvreader:
-        print(row)
+        #print(row)
+
+        total_months = total_months + 1
+        changes_in_profits = changes_in_profits + int(row[1])
+        monthchange = int(row[1]) - net
+
+        avg_of_profit.append(monthchange)
+        net = int(row[1])
+        if(greatestincrease[1] < monthchange):
+            greatestincrease[1] = monthchange
+            greatestincrease[0] = row[0]
+
+        if(greatestdecrease[1] > monthchange):
+            greatestdecrease[1] = monthchange
+            greatestdecrease[0] = row[0]
+
+profitavg = sum(avg_of_profit)/len(avg_of_profit)
 
 
-
-
+print("Total Months:")
+print(total_months)
+print(changes_in_profits)
+print(f"{profitavg:.2f}")
+print(greatestdecrease)
+print(greatestincrease)
 
 #total number of months included in dataset
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,22 +1,106 @@
 import os
 
 import csv
-
+import string
 import statistics
+print("Election Results")
 
 csvpath = os.path.join("Resources/election_data.csv")
 
 
-
+candidate1 = str("Khan")
+candidate2 = str("Correy")
+candidate3 = str("Li")
+candidate4 = str("O'Tooley")
+#temp = {}
+#name = ["Candidate"[3]]
 
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
-
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    cvotes = []
+    total_votes = 0
+    
+    
+
 
     for row in csvreader:
-        print(row)(5)
+        candidate_name = row[2]
+        total_votes = total_votes + 1
+        candidate_votes = 0
+        #cvotes = int(row[1])
+        cvotes.append(candidate_votes)
+
+
+        if (candidate_name == candidate_name):
+            candidate_name = candidate_votes + 1
+            candidate_name = row[2]
+        if (candidate_name != candidate_name):
+            candidate_name = candidate_name + 1
+            candidate_name = row[2]
+        
+
+           
+        print(candidate_name)
+        
+        #if(candidate[2] = candidate):
+            #candidate[2] = candidate + int(1)
+        
+    
+        #for candidate[0] in csvreader:
+            #name = name.lower()
+
+           # if name in csvreader:
+            #    temp[name] += 1
+     #       else:
+     #           temp[name] = 1
+     #           countnames = {}
+     #   for key, value in temp.items():
+      #      if value in countnames:
+    #            countnames[value].append(key)
+     #       else:
+     #           countnames[value] = [key]
+#print(countnames)
+
+        #if row[1] == candidate1:
+            #candidate1 = candidate1 + 1
+        #if row[1] == candidate2:
+           # candidate2 = candidate2 + 1
+        #if row[1] == candidate3:
+           # candidate3 = candidate3 + 1
+        #if row[1] == candidate4:
+           # candidate4 = candidate4 + 1
+
+   # print(candidate1 + "had" + "of the vote.")
+
+#A complete list of candidates who received votes
+
+
+#The percentage of votes each candidate won
+
+
+#The total number of votes each candidate won
+   #Khan = str(candidate_list[3])
+
+        #if (candidate_list[3] = Khan):
+            #candidate_list[3] = candidate
+            #candidate_list[0] = row[0] """
+
+
+#The winner of the election based on popular vote.
+
+
+    
+
+       # print(csvreader)(5)
+
+    #csv_header = next(csvreader)
+    #print(f"CSV Header: {csv_header}")
+
+    #for row in csvreader:
+        #print(row)(5)
+
+print("Total Votes:") 
+print(total_votes)
