213e2eaa4dae143393b9cce95ceb13b89055a392
Carrie Chunn <crchunn@gmail.com>
2020-05-21 20:10:01 -0500

    finished the code. almost done..

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,16 +3,16 @@ import os
 import csv
 import string
 import statistics
-print("Election Results")
+# print("Election Results")
 
 csvpath = os.path.join("Resources/election_data.csv")
 
 
-candidate1 = str("Khan")
-candidate2 = str("Correy")
-candidate3 = str("Li")
-candidate4 = str("O'Tooley")
-#temp = {}
+# """ candidate1 = str("Khan")
+# candidate2 = str("Correy")
+# candidate3 = str("Li")
+# candidate4 = str("O'Tooley")
+# #temp = {} """
 #name = ["Candidate"[3]]
 
 
@@ -22,28 +22,52 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     cvotes = []
     total_votes = 0
+    unique = []
+    percent = []
+    candidatecount = []
+
     
     
 
 
     for row in csvreader:
-        candidate_name = row[2]
+        # candidate_name = row[2]
         total_votes = total_votes + 1
-        candidate_votes = 0
+        # candidate_votes = 0
         #cvotes = int(row[1])
-        cvotes.append(candidate_votes)
+        # cvotes.append(candidate_votes)
 
 
-        if (candidate_name == candidate_name):
-            candidate_name = candidate_votes + 1
-            candidate_name = row[2]
-        if (candidate_name != candidate_name):
-            candidate_name = candidate_name + 1
-            candidate_name = row[2]
+        # if (candidate_name == candidate_name):
+        #     candidate_name = candidate_votes + 1
+        #     candidate_name = row[2]
+        # if (candidate_name != candidate_name):
+        #     candidate_name = candidate_name + 1
+        #     candidate_name = row[2]
+
+        if row[2] not in unique:
+            unique.append(row[2])
+
+        cvotes.append(row[2])
+
+    for candidate in unique:
+        candidatecount.append(cvotes.count(candidate))
+        percent.append(round(cvotes.count(candidate)/total_votes*100,3))
+
+    winner = unique[candidatecount.index(max(candidatecount))]
+
+print("Election Results")
+print("----------------------------")
+print(f"Total Results: {total_votes}")
+print("----------------------------")
+for i in range(len(unique)):
+    print(f"{unique[i]}: {percent[i]}% {candidatecount[i]}")
+print("----------------------------")
+print(f"Winner: {winner}")
         
 
            
-        print(candidate_votes)
+        # print(candidate_votes)
         
         #if(candidate[2] = candidate):
             #candidate[2] = candidate + int(1)
@@ -102,5 +126,5 @@ with open(csvpath) as csvfile:
     #for row in csvreader:
         #print(row)(5)
 
-print("Total Votes:") 
-print(total_votes)
+# print("Total Votes:") 
+# print(total_votes)
